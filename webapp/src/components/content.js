d52b5591a16f5c30c4ef493b2fae3c55a6d72f3e
RyanZhu <troyzx1024@gmail.com>
2017-03-19 20:52:54 -0700

    add renderActionForm function

--- webapp/src/components/content.js
+++ webapp/src/components/content.js
@@ -6,6 +6,18 @@ import {Route, Switch} from "react-router-dom";
 import TaskDetail from "./tasks/task-detail";
 import ActionForm from "./actions/action-form";
 import TaskForm from "./tasks/task-form";
+
+const renderActionForm = (match,props) => {
+	const obj = props.actions.find((act) => act.id === match.params.id);
+	if (obj) {
+		return <ActionForm onSubmit={props.changeAction}
+		                   initialValues={obj}
+		                   {...props} />
+	} else {
+		return <div>Not Found</div>
+	}
+};
+
 export default (props) => {
 	return (
 		<div>
@@ -17,7 +29,7 @@ export default (props) => {
 				<Route path="/tasks/:id/edit" render={({match}) => <TaskForm onSubmit={props.changeTask} initialValues={props.tasks.find((t) => t.id === match.params.id)} {...props} /> }/>
 				<Route path="/tasks/:id" render={({match}) => <TaskDetail task={props.tasks.find((t) => t.id === match.params.id)} {...props}/> }/>
 				{/*Don't overwrite handleSubmit in the props passed to the Form component. redux form depends on the handleSubmit event to call onSubmit. */}
-				<Route path="/actions/:id" render={({match}) => <ActionForm onSubmit={props.changeAction} initialValues={props.actions.find((act) => act.id === match.params.id)} {...props} /> } />
+				<Route path="/actions/:id" render={({match}) => renderActionForm(match, props) } />
 			</Switch>
 		</div>
 	)
