373d289eb96c8665fe96c0d9bb65053485d0589e
RyanZhu <troyzx1024@gmail.com>
2017-03-21 13:13:10 -0700

    typo

--- webapp/src/components/welcome.js
+++ webapp/src/components/welcome.js
@@ -2,12 +2,12 @@
  * Created by shuxuan on 15/03/2017.
  */
 import React from "react";
-import {Link} from 'react-router-dom';
+import {Link} from "react-router-dom";
 
 export default () => {
 	return (<div className="jumbotron">
 		<h1 className="display-3">Distributed Testing Tool Web Client</h1>
-		<p className="lead">Star building your distributed system test cases</p>
+		<p className="lead">Start building your distributed system test cases</p>
 		<hr className="my-4" />
 		<p>Support http based api testing</p>
 		<p className="lead">
