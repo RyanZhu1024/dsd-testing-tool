49d5f945ccd3e7b219606d6a487b2a2baf6c7787
RyanZhu <troyzx1024@gmail.com>
2017-03-20 20:32:09 -0700

    change title in welcome page

--- webapp/src/components/welcome.js
+++ webapp/src/components/welcome.js
@@ -6,12 +6,12 @@ import {Link} from 'react-router-dom';
 
 export default () => {
 	return (<div className="jumbotron">
-		<h1 className="display-3">Distributed Testing Tool</h1>
+		<h1 className="display-3">Distributed Testing Tool Web Client</h1>
 		<p className="lead">Star building your distributed system test cases</p>
 		<hr className="my-4" />
-			<p>Support http based api testing</p>
-			<p className="lead">
-				<Link className="btn btn-primary btn-lg" to={`actions/new`} role="button">Learn more</Link>
-			</p>
+		<p>Support http based api testing</p>
+		<p className="lead">
+			<Link className="btn btn-primary btn-lg" to={`actions/new`} role="button">Learn more</Link>
+		</p>
 	</div>);
 }
