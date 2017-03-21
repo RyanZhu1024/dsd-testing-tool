283b7033675e26f6e366b16175ca0b0338a7fae3
RyanZhu <troyzx1024@gmail.com>
2017-03-20 19:36:05 -0700

    add jumbotron

--- webapp/src/components/welcome.js
+++ webapp/src/components/welcome.js
@@ -2,7 +2,16 @@
  * Created by shuxuan on 15/03/2017.
  */
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default () => {
-	return <h1>Hello</h1>
+	return (<div className="jumbotron">
+		<h1 className="display-3">Distributed Testing Tool</h1>
+		<p className="lead">Star building your distributed system test cases</p>
+		<hr className="my-4" />
+			<p>Support http based api testing</p>
+			<p className="lead">
+				<Link className="btn btn-primary btn-lg" to={`actions/new`} role="button">Learn more</Link>
+			</p>
+	</div>);
 }
