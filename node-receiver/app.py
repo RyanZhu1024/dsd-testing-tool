de4241a1488d782338010c0e94425a8cf5905651
RyanZhu <troyzx1024@gmail.com>
2017-04-04 11:46:13 -0700

    add multi instance support

--- node-receiver/app.py
+++ node-receiver/app.py
@@ -25,8 +25,10 @@ def kill_a_process(pid):
 
 
 def send_self_info():
-    r = requests.get("http://localhost:4000/api/v1/py-info", {"port": 5000, "nodeId": "-KcoRTX-G-bGRtZVyL8w"})
-    print(r)
+    nodeIds = ["-KcoRTX-G-bGRtZVyL8w", "-KguBxT9Uq2AZ5EKfS9V", "-KguCMwQAT9UYYoMBE4n", "-KguCPVFGk5lrXSiYl9a"]
+    for node in nodeIds:
+        r = requests.get("http://localhost:4000/api/v1/py-info", {"port": 5000, "nodeId": node})
+        print(r)
 
 if __name__ == "__main__":
     send_self_info()
